399d6200a3f531f09c5bd06361224e6cb11ff424
baha <tytenov99@gmail.com>
2021-05-21 00:29:44 +0600

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,39 @@
+package com.bakhus.note
+
+import com.bakhus.note.data.collections.User
+import com.bakhus.note.data.registerUser
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing)
+    install(ContentNegotiation){
+        gson {
+            setPrettyPrinting()
+        }
+    }
+    CoroutineScope(Dispatchers.IO).launch{
+    registerUser(
+        User(
+            "abc@test.com",
+            "12345"
+        )
+    )
+    }
+
+}
+
