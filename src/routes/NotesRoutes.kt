6ccc26fea3625ce615bb01027b131d365c524580
baha <tytenov99@gmail.com>
2021-05-21 20:54:57 +0600

    done: adding noutes

--- src/routes/NotesRoutes.kt
+++ src/routes/NotesRoutes.kt
@@ -1,9 +1,12 @@
 package com.bakhus.note.routes
 
+import com.bakhus.note.data.collections.Note
 import com.bakhus.note.data.getNotesForUser
+import com.bakhus.note.data.saveNote
 import io.ktor.application.*
 import io.ktor.auth.*
 import io.ktor.http.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
@@ -18,4 +21,24 @@ fun Route.noteRoutes() {
             }
         }
     }
+
+    route("/addNote") {
+        authenticate {
+            post {
+                val note = try {
+                    call.receive<Note>()
+
+                } catch (e: ContentTransformationException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@post
+                }
+                if (saveNote(note)) {
+                    call.respond(HttpStatusCode.OK)
+                } else {
+                    call.respond(HttpStatusCode.Conflict)
+                }
+            }
+        }
+    }
+
 }
