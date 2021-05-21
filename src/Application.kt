54931df23c5165846ccc2f591193ff13005dda16
baha <tytenov99@gmail.com>
2021-05-21 20:23:35 +0600

    dont: authenticate users

--- src/Application.kt
+++ src/Application.kt
@@ -1,20 +1,15 @@
 package com.bakhus.note
 
 import com.bakhus.note.data.checkPasswordForEmail
-import com.bakhus.note.data.collections.User
-import com.bakhus.note.data.registerUser
 import com.bakhus.note.routes.loginRoute
+import com.bakhus.note.routes.noteRoutes
 import com.bakhus.note.routes.registerRoute
 import io.ktor.application.*
 import io.ktor.auth.*
+import io.ktor.auth.basic
 import io.ktor.features.*
 import io.ktor.gson.*
-import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -22,19 +17,20 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
 
+    install(Authentication) {
+        configureAuth()
+    }
     install(DefaultHeaders)
     install(CallLogging)
-    install(Routing) {
-        registerRoute()
-        loginRoute()
-    }
     install(ContentNegotiation) {
         gson {
             setPrettyPrinting()
         }
     }
-    install(Authentication) {
-        configureAuth()
+    install(Routing) {
+        registerRoute()
+        loginRoute()
+        noteRoutes()
     }
 }
 
@@ -44,11 +40,13 @@ private fun Authentication.Configuration.configureAuth() {
         realm = "Ktor Note Server"
         validate { credentials ->
             val email = credentials.name
-            val password = credentials.name
+            val password = credentials.password
             if (checkPasswordForEmail(email, password)) {
                 UserIdPrincipal(email)
             } else null
         }
     }
 }
+
+
 
---
+++ src/routes/NotesRoutes.kt
@@ -0,0 +1,21 @@
+package com.bakhus.note.routes
+
+import com.bakhus.note.data.getNotesForUser
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+fun Route.noteRoutes() {
+
+    route("/getNotes") {
+        authenticate {
+            get {
+                val email = call.principal<UserIdPrincipal>()!!.name
+                val notes = getNotesForUser(email)
+                call.respond(HttpStatusCode.OK, notes)
+            }
+        }
+    }
+}
